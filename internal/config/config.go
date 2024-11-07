afd067ac8a43cf651923c1f13cfa879ff0c297b2
DavidLSaldana <dleosaldana@gmail.com>
2024-11-07 14:56:17 -0800

    small updates during some free time to the config go file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,33 @@
 package config
 
 import (
+	"encoding/json"
 	"os"
 )
 
+const configFileName = ".gatorconfig.json"
+
 type Config struct {
-	db_url            string `db_url: "connection_string_goes_here"`
-	current_user_name string `current_user_name: "username_goes_here"`
+	Db_url   string `json:"db_url"`
+	Username string `json:"current_user_name"`
 }
 
 func Read() Config {
-	//in progress
-	file_location, _ := os.UserHomeDir()
-	os.Open(file_location + ".gatorconfig.json")
-	return Config{}
+	homeDirectory, _ := os.UserHomeDir()
+	dat, _ := os.ReadFile(homeDirectory + "/.gatorconfig.json")
+	cfg := Config{}
+
+	json.Unmarshal(dat, &cfg)
+
+	return cfg
+}
+
+// in progress
+func (cfg Config) SetUser() {
+	os.WriteFile()
+}
+
+func getConfigFilePath() (string, error) {
+	homeDirectory, err := os.UserHomeDir()
+	return homeDirectory, err
 }
