4977bc4b0414ee3053c1692fac3a082e7eeefb37
DavidLSaldana <dleosaldana@gmail.com>
2024-11-12 15:07:40 -0800

    filling out main, error in config Read

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"github.com/DavidLSaldana/gator/internal/config"
+)
+
 func main() {
+
+	cfg := config.Config{}
+
+	//error in read
+	cfg, err := config.Read()
+	if err != nil {
+		return
+	}
+
+	cfg.SetUser("David")
+
+	cfg, err = config.Read()
+	if err != nil {
+		return
+	}
+
+	fmt.Println(cfg.DBURL)
+
 }
