f0f248f7433f6f159ba7e6353942361839d7b36c
DavidLSaldana <dleosaldana@gmail.com>
2024-11-15 06:50:42 -0800

    update from a few days ago

---
+++ internal/cli/cli.go
@@ -0,0 +1,6 @@
+package cli
+
+type command struct {
+	name string
+	args []string
+}
---
+++ internal/state/state.go
@@ -0,0 +1,9 @@
+package state
+
+import (
+	"github.com/DavidLSaldana/gator/internal/config"
+)
+
+type State struct {
+	cfg *config.Config
+}
