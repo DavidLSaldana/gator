2fa660d41405f03859901c650c89522c7456ddf9
DavidLSaldana <dleosaldana@gmail.com>
2024-11-15 14:57:40 -0800

    lunch additions

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,6 +2,8 @@ package cli
 
 import (
 	"errors"
+	"fmt"
+
 	"github.com/DavidLSaldana/gator/internal/state"
 )
 
@@ -11,8 +13,30 @@ type command struct {
 }
 
 func handlerLogin(s *state.State, cmd command) error {
-	if len(cmd.args) == 0 {
-		return errors.New("Please enter an argument with this command")
+	if (len(cmd.args) == 0) || (len(cmd.args) > 1) {
+		return errors.New("Command: Login expects a single argument username")
 	}
+
+	s.Cfg.CurrentUserName = cmd.args[0]
+
+	fmt.Printf("user has been set to %s", s.Cfg.CurrentUserName)
+
+	return nil
+}
+
+type commands struct {
+	commandMap map[string]func(*state.State, command) error
+}
+
+func (c *commands) register(name string, f func(*state.State, command) error) {
+	c.commandMap[name] = f
+}
+
+func (c *commands) run(s *state.State, cmd command) error {
+	command, ok := c.commandMap[cmd.name]
+	if !ok {
+		return errors.New("Please enter a valid command")
+	}
+	//in progress
 	return nil
 }
