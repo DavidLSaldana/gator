d2381d1df20405975e61f9761faee589b611357d
DavidLSaldana <dleosaldana@gmail.com>
2025-04-07 13:49:12 -0700

    a little progress on the query for CreateFeedFollow query

--- sql/queries/createFeedFollow-InProgress.sql
+++ sql/queries/createFeedFollow-InProgress.sql
@@ -1,12 +1,28 @@
 -- name: CreateFeedFollow :one
-INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
-VALUES (
-	$1,
-	$2,
-	$3,
-	$4,
-	$5,
-)
-	#still working on this leaving off HERE
-RETURNING *, 
+
+WITH inserted_feed_follow AS (
+	INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+	VALUES (
+		$1,
+		$2,
+		$3,
+		$4,
+		$5,
+	)
+	RETURNING *
+);
+
+SELECT
+	inserted_feed_follow.*,
+	feeds.name AS feed_name,
+	users.name AS user_name
+FROM inserted_feed_follow
+
+/*
+Leaving off HERE
+INNER JOIN feeds
+ON inserted_feed_follow
+INNER JOIN
+
+*/
 
