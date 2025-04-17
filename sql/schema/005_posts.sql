d9e685c0dd6aded992b579554c4ba867b7264dbd
DavidLSaldana <dleosaldana@gmail.com>
2025-04-17 14:31:58 -0700

    starting a posts table

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,12 @@
+-- +Goose Up
+CREATE TABLE posts (
+	id INT PRIMARY KEY,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	title TEXT NOT NULL,
+	url TEXT UNIQUE NOT NULL,
+	description TEXT,
+	published_at TIMESTAMP,
+	feed_id INT NOT NULL REFERENCES feeds(id)
+		ON DELETE CASCADE
+);
